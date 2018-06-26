218759cf773100a5713275d467f97fed70ff26d0
arroyosergio <sergiof.arroyo@gmail.com>
2018-06-26 18:17:57 -0500

    cambios en el path

--- config/paths.php
+++ config/paths.php
@@ -1,14 +1,14 @@
 <?php
 
-define('URL', "http://localhost/sensontl/"); //"http://localhost/cica2016/" "http://higo-software.com/controlCongresos/"
-define('DOCS', $_SERVER['DOCUMENT_ROOT'] .'/sensontl/docs/');///congreso/docs/  /cica2016/docs/
-define('DOCSDEPOSITOSPUBLICOS', $_SERVER['DOCUMENT_ROOT'] .'/sensontl/docs/dep_publico');
-define('MENULATERAL',$_SERVER['DOCUMENT_ROOT'] .'/sensontl/views/menu/menu.html');
-define('MENUADMIN',$_SERVER['DOCUMENT_ROOT'] .'/sensontl/views/menu/menuadmin.html');
-define('MENUEDITOR',$_SERVER['DOCUMENT_ROOT'] .'/sensontl/views/menu/menueditor.html');
+define('URL', "http://localhost/cc2018/"); //"http://localhost/cica2016/" 
+define('DOCS', $_SERVER['DOCUMENT_ROOT'] .'/cc2018/docs/');///congreso/docs/  /cica2016/docs/
+define('DOCSDEPOSITOSPUBLICOS', $_SERVER['DOCUMENT_ROOT'] .'/cc2018/docs/dep_publico');
+define('MENULATERAL',$_SERVER['DOCUMENT_ROOT'] .'/cc2018/views/menu/menu.html');
+define('MENUADMIN',$_SERVER['DOCUMENT_ROOT'] .'/cc2018/views/menu/menuadmin.html');
+define('MENUEDITOR',$_SERVER['DOCUMENT_ROOT'] .'/cc2018/views/menu/menueditor.html');
 define('FECHAAPERTURA',"2018-04-01");
-define('FECHACIERRE',"2018-05-20");
-define('FECHALIMITEDESCUENTOGENERAL',"2018-05-20");
+define('FECHACIERRE',"2018-06-30");
+define('FECHALIMITEDESCUENTOGENERAL',"2018-06-30");
 
 //Cuotas de pagos 
 define('CUOTAAUTORDESCUENTO',2600.0);
@@ -17,8 +17,8 @@ define('CUOTAPUBLICODESCUENTO',2200.00);
 define('CUOTAPUBLICO',2650.0);
 
 //Fechas de descuentos
-define('FECHADESCUENTOAUTORES',"2018-05-20");
-define('FECHADESCUENTOPUBLICO',"2018-05-20");
+define('FECHADESCUENTOAUTORES',"2018-06-30");
+define('FECHADESCUENTOPUBLICO',"2018-06-30");
 
 
 
